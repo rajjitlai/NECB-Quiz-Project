451a0ad038412f253c67344565baccd476f3fef5
RAJJIT <2.0lairaj@gmail.com>
2024-08-28 12:10:27 +0530

    added-next-ui-template

---
+++ frontend/config/site.ts
@@ -0,0 +1,65 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Quizzies Quiz App",
+  description: "Sigma Quiz App for Sigma People",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Category",
+      href: "/docs",
+    },
+    {
+      label: "Leaderboard",
+      href: "/pricing",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
