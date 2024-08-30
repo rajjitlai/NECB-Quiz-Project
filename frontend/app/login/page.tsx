495d63b5184125f17872d1c71980c4fb864a8305
RAJJIT <2.0lairaj@gmail.com>
2024-08-30 11:09:27 +0530

    quiz-project-login_form_added

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import React from "react";
+import { Button, Input } from "@nextui-org/react";
+
 import { title } from "@/components/primitives";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <div>
-      <h1 className={title()}>LoginPage</h1>
+      <h1 className={title()}>Welcome Back</h1>
+      <form action="" className="mt-5">
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4 flex-col text-left">
+          {/* <label htmlFor="email">Email</label> */}
+          <Input label="Email" type="email" />
+          {/* <label htmlFor="password">Password</label> */}
+          <Input label="Password" type="password" />
+          <Button color="primary">Login</Button>
+          <div className="text-center">
+            <p>
+              Don&apos;t have an account?&nbsp;
+              <Link className="underline" href="/register">
+                Register Now
+              </Link>
+            </p>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
