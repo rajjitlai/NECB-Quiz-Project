c19cb5451801b121660d487ffb63eee4198b52b0
RAJJIT <2.0lairaj@gmail.com>
2024-09-04 12:30:53 +0530

    quiz-project-profile-right-side-added

---
+++ frontend/app/constants/index.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+export const profileStats = [
+    {
+        label: "Rank",
+        value: "1000th",
+    },
+    {
+        label: "Questions Attempted",
+        value: "500",
+    },
+    {
+        label: "Score",
+        value: "50000",
+    },
+    {
+        label: "Favorite Category",
+        value: "Programming",
+    },
+    {
+        label: "Time Spent",
+        value: "17 hours",
+    },
+    {
+        label: "Level",
+        value: "Intermediate",
+    },
+];
--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -2,7 +2,10 @@
 /* eslint-disable react/jsx-sort-props */
 "use client"
 
+import ProfileStats from "@/components/ProfileStats";
+
 import { Avatar, Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function ProfilePage() {
     return (
@@ -22,10 +25,14 @@ export default function ProfilePage() {
                     <div className="mt-10">
                         <p className="text-gray-400">Joined 1 year ago</p>
                     </div>
+
+                    <Link href="/category" className="text-xl bg-white text-black py-2 px-4 mt-10 mb-5 rounded-lg">
+                        Attempt Quiz Now!!!
+                    </Link>
                 </div>
-                <div className='lg:w-2/3 w-full bg-slate-900 pt-8 p-4 rounded-lg'>
-                    {/* Total number of quizzes attempted */}
-                    
+                <div className='lg:w-2/3 w-full pt-0 p-4 rounded-lg'>
+                    <h2 className="text-2xl lg:text-3xl font-bold">John Cena&apos;s Records</h2>
+                    <ProfileStats />
                 </div>
             </div>
         </div>
